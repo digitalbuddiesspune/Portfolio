ade530c28e3bc90dcefa37a32cf501bc8f7116ab
Swalehagameotech <zarekariswaleha@gmail.com>
2026-07-23 16:46:11 +0530

    chnaged env file

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_API_LOCAL
+    ? import.meta.env.VITE_BACKEND_API_LOCAL
     : import.meta.env.VITE_API_PROD;
 
 export default API_BASE_URL;
